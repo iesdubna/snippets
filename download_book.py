7cbfb8798507cdfa73dfa125875a2e6e62f96413
Vladimir Kozhukalov <kozhukalov@gmail.com>
2021-10-13 19:41:57 +0300

    Add requests sample

---
+++ download_book.py
@@ -0,0 +1,11 @@
+import requests
+
+link = 'https://www.gutenberg.org/files/174/174-0.txt'
+file = "picture_of_dorian_grey.txt"
+
+with requests.get(link, stream=True) as response:
+    with open(file, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+
---
+++ get_weather.py
@@ -0,0 +1,6 @@
+import requests
+
+
+resp = requests.get('https://goweather.herokuapp.com/weather/Berlin')
+data = resp.json()
+print(data["temperature"])
