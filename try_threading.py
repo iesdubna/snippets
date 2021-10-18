a362dea4c9faeaeb60160b07f7541487bc0cd0a7
Vladimir Kozhukalov <kozhukalov@gmail.com>
2021-10-18 15:39:29 +0300

    Add try_threading.py

---
+++ try_threading.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+
+def m1():
+    print("Hello. I am m1")
+    time.sleep(10)
+    print("Bye from m1")
+
+def m2():
+    print("Hello. I am m2")
+    time.sleep(2)
+    print("Bye from m2")
+
+
+t1 = threading.Thread(target=m1)
+t2 = threading.Thread(target=m2)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+
+# m1()
+# m2()
